32fe76691841b40c56d7fe43bf6e2a327d985f41
Ivan Kansazov <ivan.kansazov@smartwitness.com>
2023-01-27 16:11:32 +0200

    Fix too many requests in TestResolver
    api.macvendors accepts 1 request/sec

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -12,7 +12,7 @@ class TestResolver(unittest.TestCase):
         self.resolver = MacAddressResolver('50:ed:3c:1c:d9:86')
 
     def test_urllib_resolve_api_usage(self):
-        sleep(0.5)
+        sleep(1)
         mac_address = 'DC:9F:DB:12:DE:A3'
         _url = urljoin("https://api.macvendors.com/", quote(mac_address))
         with urllib.request.urlopen(_url) as response:
@@ -20,12 +20,12 @@ class TestResolver(unittest.TestCase):
         self.assertTrue(len(response_text) > 0)
 
     def test_mac_resolve(self):
-        sleep(0.5)
+        sleep(1)
         self.resolver.resolve()
         self.assertTrue(len(self.resolver.resolved) > 0)
 
     def test_bad_mac_resolve(self):
-        sleep(0.5)
+        sleep(1)
         mac_address = 'asd'
         resolver = MacAddressResolver(mac_address)
         with self.assertRaises(urllib.error.HTTPError):
